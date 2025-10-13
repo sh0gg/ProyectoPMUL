1fb904ab046ecf2cacf1b3db45d9392324819174
sh0gg <dbesarami@ieschandomonte.edu.es>
2025-10-13 15:14:42 +0200

    tunai is de nai

---
+++ EleccionesConLista/app/src/main/java/com/example/eleccionesconlista/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.eleccionesconlista;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private static boolean votar(int n,Candidato candidato) {
+        if (n>0) {
+            candidato.setVotos(candidato.getVotos()+1);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private static boolean login(String user,String pass) {
+        
+    }
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
