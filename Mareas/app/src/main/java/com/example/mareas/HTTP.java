f83ef2ec83a458cebe6944dc779ad1205a62d41e
sh0gg <dbesarami@ieschandomonte.edu.es>
2025-11-25 14:21:04 +0100

    eo

--- Mareas/app/src/main/java/com/example/mareas/HTTP.java
+++ Mareas/app/src/main/java/com/example/mareas/HTTP.java
@@ -1,24 +1,29 @@
 package com.example.mareas;
 
+import android.util.Log;
+
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
 import java.net.URL;
 
 public class HTTP {
     static public String getUrlContents(String strURL) {
         StringBuilder str = new StringBuilder();
+        Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("192.168.0.11", 3128));
         try {
-            HttpURLConnection con = (HttpURLConnection) new URL(strURL).openConnection();
+            HttpURLConnection con = (HttpURLConnection) new URL(strURL).openConnection(proxy);
             BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
             String linea;
             while ((linea = in.readLine()) != null) {
                 str.append(linea);
             }
         } catch (IOException e) {
-            return "";
+            return "Excepcion al abrir la URL";
         }
         return str.toString();
     }
--- Mareas/app/src/main/java/com/example/mareas/MainActivity.java
+++ Mareas/app/src/main/java/com/example/mareas/MainActivity.java
@@ -1,38 +1,46 @@
 package com.example.mareas;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.List;
-
 public class MainActivity extends AppCompatActivity {
 
-    private final String url = "https://servizos.meteogalicia.gal/mgrss/predicion/mareas/jsonMareas.action";
+    TextView tvURL;
+    EditText etURL;
+
+    //private final String url = "https://www.w3schools.com/xml/note.xml";
+    //private final String url = "https://www.duolingo.com/2017-06-30/users/";
+    private final String url = "https://servizos.meteogalicia.gal/mgrss/predicion/mareas/jsonMareas.action?idPorto=";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvURL = findViewById(R.id.tvURL);
+        etURL = findViewById(R.id.etURL);
+        etURL.setText(url);
 
         new Thread(() -> {
             String strJSONMareas = HTTP.getUrlContents(url);
-            parsearMareas(strJSONMareas);
+            tvURL.setText(strJSONMareas);
         }).start();
     }
-
-    private void parsearMareas(String strJSONMareas) {
-        try {
-            Gson gson = new Gson();
-            Type tipoMareas = new TypeToken<Mareas>() {
-            }.getType();
-            Mareas mareas = gson.fromJson(strJSONMareas, tipoMareas);
-
-            List<Marea> listaMareas = mareas.getListaMareas();
-
-        } catch (Exception e) {
-            Log.e("Error", "Error al parsear JSON", e);
-        }
-    }
 }
+
+//    private void parsearMareas(String strJSONMareas) {
+//        try {
+//            Gson gson = new Gson();
+//            Type tipoMareas = new TypeToken<Mareas>() {
+//            }.getType();
+//            Mareas mareas = gson.fromJson(strJSONMareas, tipoMareas);
+//
+//            List<Marea> listaMareas = mareas.getListaMareas();
+//
+//        } catch (Exception e) {
+//            Log.e("Error", "Error al parsear JSON", e);
+//        }
+//    }
+//}
